bb4d8b05a5cc744a5132ddffb297f08dc95a9a35
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2024-03-03 18:46:07 +0100

    add padding when focusing

--- lua/concentrate/init.lua
+++ lua/concentrate/init.lua
@@ -22,6 +22,45 @@ M.setup = function (settings)
     end
 end
 
+local function last_fold_marks()
+    return M.fold_marks[M.CURRENT_FOLD]
+end
+
+local function current_focus_size()
+
+    local last_marks = last_fold_marks()
+
+    if last_marks == nil then
+        return
+    end
+
+    local top_fold_row = va.nvim_buf_get_mark(0, last_marks[1])[1]
+    local bot_fold_row = va.nvim_buf_get_mark(0, last_marks[2])[1]
+    return bot_fold_row - top_fold_row
+
+end
+
+local function add_top_padding ()
+
+    local cur_win = va.nvim_get_current_win()
+
+    local win_height = va.nvim_win_get_height(0)
+    local focus_size = current_focus_size()
+
+    if focus_size == nil then
+        print("no focus")
+        return
+    end
+
+    local pad_height = math.floor((win_height - current_focus_size()) / 2)
+    -- create both the windows and pad them
+    -- set these to a column
+    vc("new")
+    M.TOP_PADDING = va.nvim_get_current_win()
+    va.nvim_win_set_height(0, pad_height)
+    va.nvim_set_current_win(cur_win)
+
+end
 local function unfold_row(pos)
     if vim.fn.foldlevel(pos[1]) == 0 then
         return;
@@ -75,17 +114,29 @@ local function fold_lines(top_row, bot_row)
     fold(bot_row, "$")
     -- set mark
     va.nvim_buf_set_mark(0, marks[2], bot_row, 0, {})
+    --- 
 end
 
 
 M.new_fold =  function()
+
     local selection_start = vim.fn.max({1, vim.fn.getpos("v")[2] - 1})
     local selection_end = vim.fn.min({va.nvim_win_get_cursor(0)[1] + 1, va.nvim_buf_line_count(0)})
 
+
     fold_lines(selection_start, selection_end)
 
+    if M.TOP_PADDING == nil then
+        add_top_padding()
+    end
     local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
     vim.api.nvim_feedkeys(esc, 'x', false)
+    ---
+    --
+    --
+    -- hasdfjkhasdfjh
+    --
+
 end
 
 M.remove_last_fold = function ()
@@ -122,50 +173,6 @@ M.remove_all_folds = function ()
     -- TODO remove padding windows
 end
 
-local function last_fold_marks()
-    return M.fold_marks[M.CURRENT_FOLD]
-end
-local function current_focus_size()
-
-    local last_marks = last_fold_marks()
-
-    if last_marks == nil then
-        return
-    end
-
-    local top_fold_row = va.nvim_buf_get_mark(0, last_marks[1])[1]
-    local bot_fold_row = va.nvim_buf_get_mark(0, last_marks[2])[1]
-
-    return bot_fold_row - top_fold_row
-
-end
--- bindings for dev
-vim.keymap.set({ "n", "v", "x" }, "te", function ()
-
-    -- TODO this should be get focus size
-    local cur_win = va.nvim_get_current_win()
-
-    -- get the marks rows
-    -- hsjdfhasdf
-    -- lkjasdflkj
-    -- asdflkj
-    -- asdflkj
-    local win_height = va.nvim_win_get_height(0)
-    local focus_size = current_focus_size()
-
-    if focus_size == nil then
-        print("no focus")
-        return
-    end
-
-    local pad_height = math.floor((win_height - current_focus_size()) / 2)
-    -- create both the windows and pad them
-    -- set these to a column
-    vc("new")
-    M.TOP_PADDING = va.nvim_get_current_win()
-    va.nvim_win_set_height(0, pad_height)
-    va.nvim_set_current_win(cur_win)
-end)
 
 vim.keymap.set("n", "tw" , function ()
     -- remove windows
